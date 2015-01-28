f83896cadb6c41c0f0ed426216eab6dbb9c1bef3
Weiner <weinoa11@ccm-2610-b1.evergreen.edu>
2015-01-28 11:57:54 -0800

    Start setting up game

--- Dealer.java
+++ Dealer.java
@@ -1,13 +1,31 @@
+/**
+ * Dealer.java
+ */
+
+import java.io.*;
+import java.util.*;
+
 public class Dealer {
     
-    public static void main(String[] args) {
-	// Create deck
-	Deck deck = new Deck();
-	deck.shuffle();
-	
-	// Print deck
-	for (int i = 0; i < 52; i++) {
-	    System.out.println(deck.drawCard().getID());
-	}
+  public static void main(String[] args) {
+    // Create deck
+    Deck deck = new Deck();
+    deck.shuffle();
+
+    // Set up input
+	 Scanner scan = new Scanner(System.in);
+
+    // Ask about players
+	 System.out.println("How many people are playing?");
+	 int playnum = scan.nextInt();
+
+	 // Create hands
+	 Hand dealer = new Hand();
+	 ArrayList<Hand> players = new ArrayList<Hand>();
+	 for (int i = 0; i < playnum; i++) {
+	   players.add(new Hand());
     }
+
+	 // Deal hands
+  }
 }
