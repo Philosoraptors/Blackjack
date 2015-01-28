9ba855d1809c037b7cf609c257d56aaf1c71d091
UserJeff <jeff.ladish@gmail.com>
2015-01-28 15:45:01 -0800

    Updated getScore method. We will still need to figure out how to deal with aces.

--- Hand.java
+++ Hand.java
@@ -6,19 +6,21 @@ import java.util.*;
 
 public class Hand {
     ArrayList<Card> cards;
+    int score = 0;
     
     public Hand() {
 	cards = new ArrayList<Card>();
     }
     
     public void addCard(Card c) {
-	cards.add(c);
+	    cards.add(c);
     }
     
     public int getScore() {
-	
-	return 0;
-    }
-    
+	    for (Card c : cards){
+        score += c.cardVal();   
+      }
+	    return score;
+  }  
 }
 
