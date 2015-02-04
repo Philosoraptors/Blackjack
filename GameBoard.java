3049b15dfab813ca36479d92627173eb864d3acb
UserJeff <jeff.ladish@gmail.com>
2015-02-04 11:44:21 -0800

    Added GameBoard class from wiki -- displays random cards on board

---
+++ GameBoard.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import javax.imageio.*;
+
+public class GameBoard extends JPanel {
+   static int WindowWidth = 900;
+   static int WindowHeight = 700;
+   static int Nim = 54;
+   static Random rand = new Random();
+
+static ArrayList<BufferedImage> Deck = new ArrayList<BufferedImage>();
+ 
+   public GameBoard(){
+      setPreferredSize(new Dimension(WindowWidth,WindowHeight));
+      setBackground(new Color(0, 255, 0, 128));
+   }
+ 
+  static public void loadDeck() throws java.io.IOException {
+      for (int ix = 1; ix <= Nim; ix++)
+        Deck.add(ImageIO.read(new File("classic-cards/" + ix + ".png" )));
+  }
+ 
+  static public void showCards() {
+     JFrame frame = new JFrame("BlackJack");
+     Container cnt = frame.getContentPane();
+     GameBoard pnl = new GameBoard();
+     cnt.add(pnl);
+     frame.pack();
+     frame.setVisible(true);
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+ 
+  public void paintComponent(Graphics gr){
+    BufferedImage card;
+     super.paintComponent(gr);
+     for (int ix = 1; ix < 100; ix ++ ) {
+       card = Deck.get(rand.nextInt(Nim));
+       gr.drawImage(card, rand.nextInt(WindowWidth - card.getWidth()),
+                    rand.nextInt(WindowHeight - card.getHeight()), (ImageObserver)null);
+     }
+ 
+   }
+ 
+   public static void main (String[] args) throws java.io.IOException {
+     loadDeck();
+      showCards();
+    }
+ 
+}
